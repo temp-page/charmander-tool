2a133a96c868a0692b1fd8128fa02a6066f53595
lihang941 <1329075357@qq.com>
2023-11-27 22:41:00 +0800

    feat: update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/",
+  publicPath: "./",
   devServer: {
     allowedHosts: "all",
   },
